0d481a4d4c2b17a33ba6e732ce9e1ea5b485ae4c
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2022-10-23 04:48:04 -0300

    fix: questão 4

--- questão_4/main.py
+++ questão_4/main.py
@@ -5,10 +5,22 @@ multiples_of_3_or_5 = lambda number: number % 3 == 0 or number % 5 == 0
 sum_all = lambda a, b: a + b
 
 if __name__ == "__main__":
-    numbers = list(range(0, 11))
+    flag = True
+    max_range = 0
+    while flag:
+        x_input = input("Digite um valor inteiro para X: ")        
+        if x_input.isnumeric():
+            flag = False
+            max_range = int(x_input)
+
+    numbers = list(range(1, max_range))
 
     multiples = list(filter(multiples_of_3_or_5, numbers))
     print(multiples)
+
+    if not multiples:
+        print("Sem múltiplos de 3 ou 5")
+        exit()
 
     multiples_sum = reduce(sum_all, multiples)
     print(multiples_sum)
